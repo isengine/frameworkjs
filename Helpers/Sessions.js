ef516dab6e06f55fb46cc756e3ca8262d6fbab67
fwmakc <fwmakc@mail.ru>
2021-11-03 13:11:52 +0300

    Update Sessions.js

--- Helpers/Sessions.js
+++ Helpers/Sessions.js
@@ -3,9 +3,13 @@ is.Helpers.Sessions = class {
 
 	static setCookie(name, set) {
 		document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(set) + "; path=/";
+		localStorage[name] = set;
 	}
 
 	static getCookie(name = null){
+		
+		// сборка объекта из строки кук
+		
 		let array = {};
 		document.cookie.split(";").forEach(function(item){
 			let key;
@@ -21,11 +25,53 @@ is.Helpers.Sessions = class {
 				}
 			});
 		});
-		return name ? array[name] : array;
+		
+		// создание результата из кук и локального хранилища
+		
+		let ls = name ? localStorage[name] : localStorage;
+		let result = name ? array[name] : array;
+		
+		// сравнение кук и локального хранилища
+		// куки являются приоритетными
+		
+		if (result !== ls) {
+			
+			let self = is.Helpers.Sessions;
+			
+			if (name) {
+				
+				// обновление по имени
+				
+				if (!result) {
+					self.setCookie(name, ls);
+					result = ls;
+				} else {
+					localStorage[name] = result;
+				}
+				
+			} else {
+				
+				// обновление по массиву
+				
+				Object.entries(result).forEach(function(i){
+					localStorage[ i[0] ] = i[1];
+				});
+				Object.entries(ls).forEach(function(i){
+					if (!result[ i[0] ]) {
+						self.setCookie(i[0], i[1]);
+						result[ i[0] ] = i[1];
+					}
+				});
+				
+			}
+		}
+		
+		return result;
 	}
 
 	static unCookie(name) {
 		document.cookie = encodeURIComponent(name) + "=; path=/; max-age=0";
+		delete localStorage[name];
 	}
 
 }
